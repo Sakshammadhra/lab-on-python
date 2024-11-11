652d9454235b69b122a99e2145df8611869584aa
Sakshammadhra <saksham.madhra@gmail.com>
2024-11-10 22:09:41 -0800

    Update Q66 create and modify a dictionary.py

--- Q66 create and modify a dictionary.py
+++ Q66 create and modify a dictionary.py
@@ -3,4 +3,4 @@ my_dict = {'name': 'Alice', 'age': 25}
 my_dict['age'] = 26
 my_dict['city'] = 'New York'
 print(my_dict) # Outputs: {'name': 'Alice', 'age': 26, 'city': 'New York'}
-print("program by Diya Bhutani- 0221BCA041")
+print("Program by Saksham Madhra- 0221BCA164")
