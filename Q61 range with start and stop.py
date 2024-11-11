da735c5ce0bf3a9d1bcfef5980e1cd35806c4506
Sakshammadhra <saksham.madhra@gmail.com>
2024-11-10 22:10:45 -0800

    Update Q61 range with start and stop.py

--- Q61 range with start and stop.py
+++ Q61 range with start and stop.py
@@ -1,4 +1,4 @@
 #Program to Use range() with Start and Stop:
 for i in range(2, 6):
  print(i) # Outputs: 2, 3, 4, 5
-print("program by Diya Bhutani- 0221BCA041")
+print("Program by Saksham Madhra- 0221BCA164")
