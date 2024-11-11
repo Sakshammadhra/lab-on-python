b230d547fc847e4fd98ed44e55f305f82db54d54
Sakshammadhra <saksham.madhra@gmail.com>
2024-11-10 21:59:04 -0800

    Update Q93.py

--- Q93.py
+++ Q93.py
@@ -2,4 +2,4 @@ import importlib
 math_utils = importlib.import_module('math_utils')
 result = math_utils.add(5, 3)
 print(result)
-print("Program by Diya Bhutani- 0221BCA041")
+print("Program by Saksham Madhra- 0221BCA164")
