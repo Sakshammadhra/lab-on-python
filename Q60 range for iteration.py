95ec21b4c8cbb178682e7af2daa895a73c18cc55
Sakshammadhra <saksham.madhra@gmail.com>
2024-11-10 22:10:56 -0800

    Update Q60 range for iteration.py

--- Q60 range for iteration.py
+++ Q60 range for iteration.py
@@ -1,4 +1,4 @@
 #Program to Use range() for Iteration:
 for i in range(5):
  print(i) # Outputs: 0, 1, 2, 3, 4
-print("program by Diya Bhutani- 0221BCA041")
+print("Program by Saksham Madhra- 0221BCA164")
